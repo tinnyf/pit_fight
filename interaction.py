b92dabd06e5bd8e340f3052ea6f7e9cdac5b0da0
tinnyf <65498001+tinnyf@users.noreply.github.com>
2023-02-08 21:00:17 +0000

    Add basic tests for opposite

--- interaction.py
+++ interaction.py
@@ -21,6 +21,27 @@ class Interaction:
         return self.get_plus_one(self,position)
 
     def opposite(self, position):
+        return(position)
 
     def right(self,position):
         return self.get_minus_one(self.position)
+
+
+    def get_character_at_position(self, position):
+        pass
+
+
+
+
+
+
+
+def tests():
+    opposite_tests()
+
+
+def opposite_tests():
+    assert Interaction.opposite(1) == 4, "Output should be 4"
+    assert Interaction.opposite(2) == 7, "Output should be 7"
+    assert Interaction.opposite(3) == 6, "Output should be 6"
+    assert Interaction.opposite(0) == 5, "Output be 5"
