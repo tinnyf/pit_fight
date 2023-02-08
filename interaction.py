f60eb4c59dd44820d9b98333a075d5b34d254723
Matt Kershaw <matt@kershaw.org>
2023-02-08 21:22:18 +0000

    Fix code weirdness

--- interaction.py
+++ interaction.py
@@ -1,24 +1,21 @@
 class Interaction:
-
     def __init__(self):
         self.characters = {}
 
-
     def get_plus_one(self, position):
         position = position + 1
-        if position < 7: #0 indexed list and 8 possible positions.
+        if position < 7:  # 0 indexed list and 8 possible positions.
             return position - 7
         return position
 
-
     def get_minus_one(self, position):
         position -= 1
-        if position >0:
-            return position + 8 # -1 == 7
+        if position > 0:
+            return position + 8  # -1 == 7
         return position
 
     def left(self, position):
-        return self.get_plus_one(self,position)
+        return self.get_plus_one(position)
 
     def get_pair(self, position):
         return position // 2
@@ -27,11 +24,10 @@ class Interaction:
         return (self.get_pair(position) + 2) % 4
 
     def opposite(self, position):
-        return(position)
+        return position
 
-    def right(self,position):
-        return self.get_minus_one(self.position)
-
+    def right(self, position):
+        return self.get_minus_one(position)
 
     def get_character_at_position(self, position):
         pass
